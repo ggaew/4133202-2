a77cb6e2ccf34bea0249b61934aa7fac6f9a156b
ggaew <gaew.thanyaphonfang2542@gmail.com>
2019-07-12 15:34:44 +0700

    init project

---
+++ php/add.php
@@ -0,0 +1,18 @@
+<?php
+echo $_POST['user'];
+echo "Hello Word !!!";
+
+$name = "THANYAPHON";
+echo "Miss." . $name;
+echo "<br>";
+
+
+echo sum(5,3);
+
+function sum($A ,$B){
+    return $A + $B;
+
+}
+?>
+//short term, use one line only.
+<?='My name is Gaew'?>
